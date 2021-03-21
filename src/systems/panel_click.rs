544babb22af746fdb4e02d06e4b3798f3e7f70de
TuntematonSotilas <batault_gael@msn.com>
2021-03-21 15:20:51 +0100

    pnel

--- src/systems/panel_click.rs
+++ src/systems/panel_click.rs
@@ -7,6 +7,7 @@ pub struct PanelClickSystem;
 impl<'s> System<'s> for PanelClickSystem {
     type SystemData = (
 		Read<'s, InputController>,
+		Read<'s, CompositeUiInteractibles>,
 		ReadStorage<'s, CompositeUiElement>,
 		Read<'s, CompositeCameraCache>,
 		Read<'s, Camera>,
@@ -14,24 +15,24 @@ impl<'s> System<'s> for PanelClickSystem {
 
     fn run(
         &mut self,
-		(input, ui_elements, camera_cache, camera_res)
+		(input, interactibles, ui_elements, camera_cache, camera_res)
 		: Self::SystemData,
     ) {
        if input.trigger_or_default("mouse-left") == TriggerState::Pressed {
 			let x = input.axis_or_default("mouse-x");
             let y = input.axis_or_default("mouse-y");
-			//let point = [x, y].into();
-			
-			if let Some(camera_entity) = camera_res.camera {
+			let point = [x, y].into();
+
+			if let Some(camera_entity) = camera_res.camera_panel {
 				for ui_element in (ui_elements).join() {	
-					for child in &ui_element.children {  
-						if let Some(id) = &child.id {
-							if id == "item" {
-								if let Some(w) = &child.fixed_width {
-									//debug!("{0}", w);
-								}
-								
-								
+					if let Some(inter_name) = &ui_element.interactive {
+
+						if let Some(pos) = camera_cache.screen_to_world_space(camera_entity, point) {
+							if interactibles.does_rect_contains_point(inter_name, pos) {
+
+								debug!("{0}", inter_name);
+
+
 							}
 						}
 					}
