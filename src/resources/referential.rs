770a3320c893271704f533d6c71cb29144a5f7b6
TuntematonSotilas <batault_gael@msn.com>
2021-03-25 19:16:34 +0100

    Referential

--- src/resources/referential.rs
+++ src/resources/referential.rs
@@ -1,12 +1,24 @@
+use std::collections::HashMap;
+
+#[derive(Clone)]
+pub enum ItemType {
+	Unit,
+	Building,
+	Upgrade
+}
+
+impl Default for ItemType {
+    fn default() -> Self { ItemType::Unit }
+}
+
 #[derive(Default, Clone)]
 pub struct Item  {
-    pub code: String,
     pub name: String,
+	pub itype: ItemType,
     pub cost: u32,
 }
 
 #[derive(Default, Clone)]
 pub struct Referential {
-    pub units: Vec<Item>,
-	pub upgrades: Vec<Item>,
+	pub items: HashMap<String, Item>
 }
--- src/states/splash.rs
+++ src/states/splash.rs
@@ -1,6 +1,9 @@
 use oxygengine::prelude::*;
 
-use crate::states::intro::IntroState;
+use crate::{
+	states::intro::IntroState,
+	resources::referential::{Referential, ItemType},
+};
 
 #[derive(Default)]
 pub struct SplashState;
@@ -15,6 +18,14 @@ impl State for SplashState {
     }
 
     fn on_process(&mut self, world: &mut World) -> StateChange {
+
+		/*let items: HashMap::new()
+		let referential = Referential {
+			itype: 
+		};
+
+		world.write_resource::<Referential>();*/
+
         let input = &world.read_resource::<InputController>();
         if input.trigger_or_default("enter") == TriggerState::Pressed {
             return StateChange::Swap(Box::new(IntroState));
