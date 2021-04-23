c83ec3021ac0cf17681e68ba79b09a2022ad5731
TuntematonSotilas <batault_gael@msn.com>
2021-04-23 17:57:49 +0200

    panel

--- src/ui/components/resources.rs
+++ src/ui/components/resources.rs
@@ -5,16 +5,44 @@ use oxygengine::user_interface::raui::{
 
 widget_component! {
     pub resources_comp(key, props, state) {
-		let bkg = props.clone()
-            .with(PaperProps { frame: None, ..Default::default() });
-		let size = Props::new(SizeBoxProps {
+		let bkg = PaperProps { 
+			frame: None, 
+			..Default::default() 
+		};
+		let bkg_size = Props::new(SizeBoxProps {
             width: SizeBoxSizeValue::Fill, 
             height: SizeBoxSizeValue::Exact(30.),
             ..Default::default()
         });
+		let bkg_margin = Props::new(ContentBoxItemLayout {
+			margin: Rect {
+				top: 5.,
+				..Default::default()
+			},
+			..Default::default()
+		});
+		let res_bkg = PaperProps { 
+			variant: "data".to_string(),
+			frame: None, 
+			..Default::default() 
+		};
+		let res_size = Props::new(SizeBoxProps {
+            width: SizeBoxSizeValue::Exact(80.), 
+            height: SizeBoxSizeValue::Exact(20.),
+            ..Default::default()
+        });
+
 		widget!{
-			(#{"size"} size_box: {size} {
-				content = (#{"paper"} paper: {bkg})
+			(#{"bkg-size"} size_box: {bkg_size} {
+				content = (#{"bkg"} paper: {bkg} [
+					(#{"bkg-cnt"} content_box [
+						(#{"bkg-margin"} content_box : {bkg_margin} [
+							(#{"res-size"} size_box: {res_size} {
+								content = (#{"res-bkg"} paper: {res_bkg})
+							})
+						])
+					])
+				])
 			})
 		}
 	}
