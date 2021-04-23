9f9202b6064fbdec9637b72e7692e467b7906621
G.Batault <g.batault@citegestion.fr>
2021-04-23 16:52:20 +0200

    panel

--- src/ui/gui/gui_game.rs
+++ src/ui/gui/gui_game.rs
@@ -29,30 +29,31 @@ pub fn theme_game() -> ThemeProps {
         &mut theme.text_variants,
     );
 
-    make_bkg_variants(
+    /*make_bkg_variants(
     "",
     ThemedImageMaterial::Image(ImageBoxImage {
-        id: "ui/panel_bkg.png".to_owned(),
-        ..Default::default()
-        }),
+            id: "ui/panel_bkg.png".to_owned(),
+            ..Default::default()
+            }),
     &mut theme.content_backgrounds,
-    );
+    );*/
 
-    /*let background_props2 = Props::new(ImageBoxProps {
-			material: ImageBoxMaterial::Image(ImageBoxImage {
-			  id: "image.png".to_owned(),
-			  scaling: ImageBoxImageScaling::Frame(ImageBoxFrame {
+    make_bkg_variants(
+        "",
+        ThemedImageMaterial::Image(ImageBoxImage {
+            id: "ui/panel_bkg.png".to_owned(),
+            scaling: ImageBoxImageScaling::Frame(ImageBoxFrame {
 				// each field of a rect defines the size of a frame on each side of the source image
-				source: Rect { left: 16.0, right: 16.0, top: 16.0, bottom: 16.0},
+				source: Rect { left: 3.0, right: 3.0, top: 3.0, bottom: 3.0},
 				// destination maps source rect frame to the ui space values so if you don't want to "rescale" the frame just make it the same as source.
 				// if you double the values here then all frame sides would have doubled size in ui space.
-				destination: Rect { left: 16.0, right: 16.0, top: 16.0, bottom: 16.0},
+				destination: Rect { left: 3.0, right: 3.0, top: 3.0, bottom: 3.0},
 				..Default::default()
 			  }),
-			  ..Default::default()
-			}),
-			..Default::default()
-		  });*/
+            ..Default::default()
+        }),
+        &mut theme.content_backgrounds,
+    );
 
     theme
 }
